11e2e6363f31884945f3e36606eabd4a36aab76e
Watanabe, Katsutoshi <Watanabe.Katsutoshi@mss.co.jp>
2015-01-09 12:02:31 +0900

    treewidgetitem.py を分離

--- pygantt/widget.py
+++ pygantt/widget.py
@@ -11,6 +11,7 @@ from .util import s2dt, dt2s
 from .settings import *
 from .model import Task, TaskModel
 from .config import config
+from .treewidgetitem import TreeWidgetItem
 
 _ONEDAY = timedelta(days=1)
 
@@ -509,62 +510,3 @@ class GanttWidget(Widget_):
 
     def taskCollapsed(self, item):
         item.task.expanded = False
-
-
-class TreeWidgetItem(QtGui.QTreeWidgetItem):
-    def __init__(self, task=None):
-        super(TreeWidgetItem, self).__init__()
-        if task is not None:
-            self.taskToItem(task)
-
-    def taskToItem(self, task):
-        self.name = task.name
-        self.start = dt2s(task.start)
-        self.end = dt2s(task.end)
-        self.setText(3, "unknown")
-        self.task = task
-        self.setFlags(self.flags() | Qt.ItemIsEditable)
-        if task.children is not None and len(task.children) > 0:
-            self.addChildren(TreeWidgetItem.Items(task.children))
-
-    def childItems(self):
-        items = []
-        for i in range(self.childCount()):
-            items.append(self.child(i))
-        return items
-
-    @property
-    def task(self):
-        return self.data(COLUMN_CHART, Qt.UserRole)
-
-    @task.setter
-    def task(self, value):
-        self.setData(COLUMN_CHART, Qt.UserRole, value)
-
-    @property
-    def name(self):
-        return self.data(0, Qt.DisplayRole)
-
-    @name.setter
-    def name(self, value):
-        self.setData(0, Qt.DisplayRole, value)
-
-    @property
-    def start(self):
-        return self.data(1, Qt.DisplayRole)
-
-    @start.setter
-    def start(self, value):
-        self.setData(1, Qt.DisplayRole, value)
-
-    @property
-    def end(self):
-        return self.data(2, Qt.DisplayRole)
-
-    @end.setter
-    def end(self, value):
-        self.setData(2, Qt.DisplayRole, value)
-
-    @staticmethod
-    def Items(tasks):
-        return [TreeWidgetItem(tasks[i]) for i in range(0, len(tasks))]
